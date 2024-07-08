86ca31d1bd8ee1eab772467e9b45869166e88916
EvZb <130612260+EvZb@users.noreply.github.com>
2024-07-07 20:26:13 -0700

    fix material rarities

--- content/menu/items.js
+++ content/menu/items.js
@@ -1,6 +1,6 @@
 (function () {let $0=[];let $1=[];let $2=``;let $3=[]
     itm.slice(1).forEach(($a,$b)=>{let $c=arr('item',$b+1);$0.push($a+":"+(1+$b));$1.push(col(cc[$c[3]],$a)+` [ ${itype[$c[2]]} ] (${$c[1]})`)});
-    mat.slice(1).forEach(($a,$b)=>{let $c='';if(matf[$b]!=0){$c+=matform[maf[$b]]+' of '};`${$c+=col(matr[$b],$a)} [${afftype[matt[$b]]}] (${mata[$b]})`;$3.push($c)});
+    mat.slice(1).forEach(($a,$b)=>{let $c='';if(matf[$b]!=0){$c+=matform[maf[$b]]+' of '};`${$c+=col(cc[matr[$b]-1],$a)} [${afftype[matt[$b]]}] (${mata[$b]})`;$3.push($c)});
     $2=`Item rarity colors (rarest to least rare): `;cc.forEach(($a,$b)=>{$2+=`\${col('${$a}',"${$b+1}")} `})
     if(combat=='F'){$2+=`<br><br>\${btn('use',"iuse(elm('itmes').value);next('menu/items',0)")} \${lst('items',"${$0.join(',')}")} \${btn('destroy',"rem('item',elm('items').value);next('menu/items',0)")}`}
     else{$2+=`<br><br>\${col('${cc[0]}','Item interactions are disabled during combat.')}`}
